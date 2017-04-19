4b97fc62dae1225765f67778f681e6ed90555610
hardd2007 <hardd2007@github.com>
2017-04-19 22:38:51 +0300

    Add MR3020 device in tree

---
+++ MR3020files/files/etc/diag.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+. /lib/functions/leds.sh
+get_status_led() {
+status_led="tp-link:green:wps"
+}
+set_state() {
+get_status_led
+case "$1" in
+preinit)
+status_led_blink_preinit
+;;
+failsafe)
+status_led_blink_failsafe
+;;
+upgrade | \
+preinit_regular)
+status_led_blink_preinit_regular
+;;
+done)
+status_led_set_heartbeat
+#status_led_on
+esac
+}
